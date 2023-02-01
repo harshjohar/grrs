2856ed1ea3e797135ee513f0cafe4517070aa350
harsh johar <hpsj3112@gmail.com>
2023-02-01 12:56:49 +0530

    lil error handling

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,22 @@ use clap::Parser;
 
 struct Cli {
     pattern: String,
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let result = std::fs::read_to_string("test.txt");
+    let content = match result {
+        Ok(content) => {
+            content
+        }
+        Err(error) => {
+             panic!("Can't deal with {}, just exit here", error); 
+             // or maybe
+        }
+    };
+
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line.trim());
